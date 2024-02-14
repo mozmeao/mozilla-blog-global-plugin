7012343d2a8dc7c3c0c31758949f81658e9d991d
Stephanie Hobson <sjhobson@gmail.com>
2024-02-14 12:39:58 -0800

    Update snippet to make gtag available globally

--- mozilla-custom/ga-snippet.js
+++ mozilla-custom/ga-snippet.js
@@ -76,11 +76,11 @@ Mozilla.dntEnabled = function(dnt, ua) {
         (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
         new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
         j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        'https://www.googletagmanager.com/gtag/js?id='+i+dl;f.parentNode.insertBefore(j,f);
         })(window,document,'script','dataLayer',measurementId);
 
         window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
+        window.gtag = function(){dataLayer.push(arguments);}
         gtag('js', new Date());
 
         gtag('config', measurementId);
